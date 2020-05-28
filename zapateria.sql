640cdd4be98df4b313c9588d60b9158b6a48843c
IsaiasRVH <65567240+IsaiasRVH@users.noreply.github.com>
2020-05-27 19:57:37 -0500

    Add files via upload

---
+++ zapateria.sql
@@ -0,0 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-05-2020 a las 02:57:21
+-- Versión del servidor: 5.7.17
+-- Versión de PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `zapateria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idCliente` int(11) NOT NULL,
+  `nombre` varchar(25) COLLATE utf8_bin NOT NULL,
+  `apellidos` varchar(25) COLLATE utf8_bin NOT NULL,
+  `calleYNumero` varchar(40) COLLATE utf8_bin NOT NULL,
+  `colonia` varchar(25) COLLATE utf8_bin NOT NULL,
+  `ciudad` varchar(25) COLLATE utf8_bin NOT NULL,
+  `codigoPostal` varchar(5) COLLATE utf8_bin NOT NULL,
+  `estado` varchar(20) COLLATE utf8_bin NOT NULL,
+  `pais` varchar(20) COLLATE utf8_bin NOT NULL,
+  `telefono` varchar(13) COLLATE utf8_bin NOT NULL,
+  `email` varchar(30) COLLATE utf8_bin NOT NULL,
+  `monto` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalleventa`
+--
+
+CREATE TABLE `detalleventa` (
+  `idVenta` int(11) NOT NULL,
+  `codigo` varchar(20) COLLATE utf8_bin NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` double NOT NULL,
+  `importe` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `codigo` varchar(25) COLLATE utf8_bin NOT NULL,
+  `modelo` varchar(40) COLLATE utf8_bin NOT NULL,
+  `marca` varchar(20) COLLATE utf8_bin NOT NULL,
+  `color` varchar(30) COLLATE utf8_bin NOT NULL,
+  `estilo` varchar(15) COLLATE utf8_bin NOT NULL,
+  `existencias` double NOT NULL,
+  `precio` double NOT NULL,
+  `idProveedor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `idProveedor` int(11) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_bin NOT NULL,
+  `calleYNumero` varchar(40) COLLATE utf8_bin NOT NULL,
+  `colonia` varchar(25) COLLATE utf8_bin NOT NULL,
+  `ciudad` varchar(25) COLLATE utf8_bin NOT NULL,
+  `codigoPostal` varchar(5) COLLATE utf8_bin NOT NULL,
+  `estado` varchar(25) COLLATE utf8_bin NOT NULL,
+  `pais` varchar(25) COLLATE utf8_bin NOT NULL,
+  `telefono` varchar(13) COLLATE utf8_bin NOT NULL,
+  `email` varchar(30) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `nombre` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `apellidos` varchar(25) COLLATE utf8mb4_bin NOT NULL,
+  `telefono` varchar(13) COLLATE utf8mb4_bin NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `calleYNumero` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `colonia` varchar(15) COLLATE utf8mb4_bin NOT NULL,
+  `codigoPostal` varchar(5) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `ciudad` varchar(15) COLLATE utf8mb4_bin NOT NULL,
+  `estado` varchar(15) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `pais` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `salario` double NOT NULL,
+  `contraseña` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `idVenta` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `total` double NOT NULL,
+  `tipoVenta` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indices de la tabla `detalleventa`
+--
+ALTER TABLE `detalleventa`
+  ADD PRIMARY KEY (`idVenta`,`codigo`),
+  ADD KEY `codigo` (`codigo`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`codigo`),
+  ADD KEY `idProveedor` (`idProveedor`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`idProveedor`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`idVenta`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idCliente` (`idCliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `idProveedor` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `idVenta` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalleventa`
+--
+ALTER TABLE `detalleventa`
+  ADD CONSTRAINT `detalleventa_ibfk_1` FOREIGN KEY (`codigo`) REFERENCES `producto` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `detalleventa_ibfk_2` FOREIGN KEY (`idVenta`) REFERENCES `venta` (`idVenta`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`idProveedor`) REFERENCES `proveedor` (`idProveedor`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
