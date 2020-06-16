e2d6b840114488c7eec071ef2d6acc13cfe12095
IsaiasRVH <65567240+IsaiasRVH@users.noreply.github.com>
2020-06-15 23:12:42 -0500

    Add files via upload

---
+++ Punto De Venta/src/Reportes/GenerarReporte.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Reportes;
+
+import DAO.DAOException;
+import MySQLConection.Conectar;
+import java.awt.Frame;
+import java.sql.Connection;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+
+/**
+ *
+ * @author Isaías Ricardo Valdivia Hernández
+ */
+public class GenerarReporte {
+    //La conexion a la base de datos
+    private Connection conn;
+    
+    
+    
+    public GenerarReporte(int idUsuario,int idVenta,int idCliente, Double pago) throws DAOException{
+          try {
+            JasperReport reporte = null;
+            String path = "src\\Reportes\\Ticket.jasper";
+            
+            reporte = (JasperReport) JRLoader.loadObjectFromFile(path);
+           
+            conectar();
+            
+            JasperPrint jprint = JasperFillManager.fillReport(reporte, null, conn);
+            
+            JasperViewer view = new JasperViewer(jprint, false);
+            
+            JDialog dialog = new JDialog((Frame) null,true);
+            dialog.setContentPane(view.getContentPane());
+            dialog.setSize(view.getSize());
+            dialog.setTitle("Ticket de Venta");
+            dialog.setVisible(true);
+            
+            desconectar();
+        } catch (JRException ex) {
+            throw new DAOException("No se ha podido generar el ticket.");
+        }
+    }
+    
+    
+    public void conectar() throws DAOException {
+        conn = Conectar.realizarConexion();
+    }
+    
+    
+    public void desconectar() throws DAOException {
+        Conectar.desconectarConnection(conn);
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+}
