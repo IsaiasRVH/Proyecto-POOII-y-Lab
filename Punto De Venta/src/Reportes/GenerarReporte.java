caa6417b76829990a783068537af9e1fa375f07f
IsaiasRVH <65567240+IsaiasRVH@users.noreply.github.com>
2020-06-19 21:20:10 -0500

    Update GenerarReporte.java

--- Punto De Venta/src/Reportes/GenerarReporte.java
+++ Punto De Venta/src/Reportes/GenerarReporte.java
@@ -34,27 +34,36 @@ public class GenerarReporte {
     
     public GenerarReporte(int idVenta, Double pago, String tipo) throws DAOException{
           try {
+            //Variable para generar el reporte
             JasperReport reporte = null;
+            //Ruta donde se encuentra el reporte
             String path = "src\\Reportes\\Ticket" + tipo + ".jasper";
             
+            //Se carga el reporte del archivo
             reporte = (JasperReport) JRLoader.loadObjectFromFile(path);
            
+            //Se establece conexion con la base de datos
             conectar();
             
+            //Se crea el Map y se agregan los parametros necesarios
             Map parametros = new HashMap();
             parametros.put("idVenta", idVenta);
             parametros.put("pago", pago);
             
+            //Se crea el JasperPrint con el reporte cargado, los parametros y la conexion
             JasperPrint jprint = JasperFillManager.fillReport(reporte, parametros, conn);
             
+            //Se crea la vista para el reporte
             JasperViewer view = new JasperViewer(jprint, false);
             
+            //La vista se asigna a un JDialog para que este hasta el frente de las ventanas
             JDialog dialog = new JDialog((Frame) null,true);
             dialog.setContentPane(view.getContentPane());
             dialog.setSize(view.getSize());
             dialog.setTitle("Ticket de Venta");
             dialog.setVisible(true);
             
+            //Se cierra la conexion con la base de datos
             desconectar();
         } catch (JRException ex) {
             throw new DAOException("No se ha podido generar el ticket.");
@@ -63,15 +72,13 @@ public class GenerarReporte {
     
     
     public void conectar() throws DAOException {
+        //Se obtiene una conexion a la base de datos
         conn = Conectar.realizarConexion();
     }
     
     
     public void desconectar() throws DAOException {
+        //Se cierra la conexxion con la base de datos
         Conectar.desconectarConnection(conn);
-    }
-
-    public Connection getConn() {
-        return conn;
     }
 }
